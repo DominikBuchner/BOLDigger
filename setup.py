6fdc84dcedd09884f1db43b377ed23668ffc0dfc
DominikBuchner <38790188+DominikBuchner@users.noreply.github.com>
2020-04-15 10:45:43 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="boldigger",
+    version="1.0.0",
+    author="Dominik Buchner",
+    author_email="dominik.buchner524@googlemail.com",
+    description="A python package to query different databases of boldsystems.org",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DominikBuchner/BOLDigger",
+    packages=setuptools.find_packages(),
+    license = 'MIT',
+    install_requires = ['PySimpleGUI', 'requests-html', 'beautifulsoup4', 'openpyxl',
+                        'numpy', 'pandas', 'requests', 'more-itertools'],
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points = {
+        "console_scripts" : [
+            "boldigger = boldigger.__main__:main",
+        ]
+    },
+)
