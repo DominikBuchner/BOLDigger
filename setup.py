9d85e04017e996a82f028785ec146f9f4a152505
lokalmatador123 <38790188+lokalmatador123@users.noreply.github.com>
2020-04-21 17:44:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="boldigger",
-    version="1.1.1",
+    version="1.1.2",
     author="Dominik Buchner",
     author_email="dominik.buchner524@googlemail.com",
     description="A python package to query different databases of boldsystems.org",
